5a2a41f5fa5f40ad6f1cc56f3f1614e1d7c497d4
CorrM <islamnofl.official@gmail.com>
2024-07-17 06:33:00 +0300

    Add gaming\nFix waybar bluetooth

---
+++ gaming/.config/gaming/scripts/gamemode_end.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+notify-send "GameMode ended"
+mpvpaper '*' -s -o "no-audio --loop-playlist" ~/.config/mpvpaper/wallpaper/wallpaper.mp4
---
+++ gaming/.config/gaming/scripts/gamemode_start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+notify-send "GameMode started"
+killall mpvpaper
